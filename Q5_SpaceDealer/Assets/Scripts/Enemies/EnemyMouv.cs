8143edecb799ac03f34189e1cc3e2a5ead5479ba
TututLaHorde <a.boucher@lahorde.school>
2024-01-09 14:41:16 +0100

    Avoiding

--- Q5_SpaceDealer/Assets/Scripts/Enemies/EnemyMouv.cs
+++ Q5_SpaceDealer/Assets/Scripts/Enemies/EnemyMouv.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMouv : MonoBehaviour
@@ -6,17 +5,18 @@ public class EnemyMouv : MonoBehaviour
     [Header("params")]
     [SerializeField] private float m_speed;
 
-    [Header("FOV")]
-    [SerializeField] private float m_avoidingArea;
-    [SerializeField] private float m_cohesionArea;
+    [Header("Behavior Coeff")]
+    [SerializeField][Range(0f, 1f)] private float m_avoidCoeff;
+    [SerializeField][Range(0f, 1f)] private float m_cohesionCoeff;
+
+    [Header("Behavior Range")]
+    [SerializeField] private float m_avoidingRange;
+    [SerializeField] private float m_cohesionRange;
     [SerializeField] private LayerMask m_avoidingLayers;
     [SerializeField] private LayerMask m_cohesionLayers;
 
     //own components
     private Rigidbody2D m_rb;
-
-    //utilities
-    private List<GameObject> m_avoidingGO;
 
     private void Start()
     {
@@ -25,13 +25,13 @@ public class EnemyMouv : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Avoiding();
-        Cohesion();
-        MoveFoward();
+        m_rb.velocity += Avoiding();
+
+        //MoveFoward();
 
         //debug areas
-        Debug.DrawRay(transform.position, Vector2.up * m_avoidingArea, Color.red);
-        Debug.DrawRay(transform.position, Vector2.down * m_avoidingArea, Color.cyan);
+        Debug.DrawRay(transform.position, Vector2.up * m_avoidingRange, Color.red);
+        Debug.DrawRay(transform.position, Vector2.down * m_cohesionRange, Color.cyan);
     }
 
     private void MoveFoward()
@@ -41,23 +41,64 @@ public class EnemyMouv : MonoBehaviour
         m_rb.velocity = vec * m_speed;
     }
 
-    private void Avoiding()
+    private Vector2 Avoiding()
     {
-        //reset previous check
-        m_avoidingGO.Clear();
+        int nbCloseObj = 0;
+        Vector2 avoidVelocity = Vector2.zero;
 
         //find all object to avoid
-        foreach (Collider2D coll in Physics2D.OverlapCircleAll(transform.position, m_avoidingArea, m_avoidingLayers))
+        foreach (Collider2D coll in Physics2D.OverlapCircleAll(transform.position, m_avoidingRange, m_avoidingLayers))
         {
             if (coll.gameObject != gameObject)
             {
-                m_avoidingGO.Add(coll.gameObject);
+                //specific avoid velocity
+                float dist = Vector2.Distance((Vector2)transform.position, coll.ClosestPoint(transform.position));
+                Vector2 dir = (Vector2)transform.position - coll.ClosestPoint(transform.position);
+                dir = dir.normalized;
+
+                avoidVelocity += dir / dist; //closer is harder
+                nbCloseObj++;
             }
         }
+
+        //global avoid velocity
+        if (nbCloseObj > 0)
+        {
+            avoidVelocity /= nbCloseObj;
+            avoidVelocity *= m_avoidCoeff;
+        }
+
+        return avoidVelocity;
     }
 
-    private void Cohesion()
+    private float CohesionAngle()
     {
+        return transform.rotation.eulerAngles.z;
 
+        int nbCloseObj = 0;
+        Vector2 averagePoints = Vector2.zero;
+
+        //find all object to avoid
+        foreach (Collider2D coll in Physics2D.OverlapCircleAll(transform.position, m_avoidingRange, m_avoidingLayers))
+        {
+            if (coll.gameObject != gameObject)
+            {
+                averagePoints += coll.ClosestPoint(transform.position);
+                nbCloseObj++;
+            }
+        }
+
+        //set the necessary direction to avoid
+        if (nbCloseObj > 0)
+        {
+            averagePoints /= nbCloseObj;
+
+            float o = averagePoints.x - transform.position.x;
+            float a = averagePoints.y - transform.position.y;
+            float angle = 180 - Mathf.Atan2(o, a) * Mathf.Rad2Deg;
+            return angle;
+        }
+
+        return transform.rotation.eulerAngles.z;
     }
 }
